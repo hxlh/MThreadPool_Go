a6e5faf8d9916d52e4693f4f784354d64d133d26
hxlh <49110117+hxlh@users.noreply.github.com>
2020-11-19 13:38:36 +0800

    Add files via upload

--- main.go
+++ main.go
@@ -6,24 +6,46 @@ import (
 	"time"
 )
 
-func test(args interface{}) interface{} {
-	time.Sleep(time.Millisecond * 1000)
-	fmt.Println(args)
-	return ""
+type Test struct {
+}
+
+func (tt *Test) test(args interface{}, ch chan int) interface{} {
+	// time.Sleep(time.Millisecond * 1000)
+	// fmt.Println(args)
+	// return ""
+	for {
+		select {
+		case <-ch:
+			//do something…………
+			//跳出循环，退出
+			goto loop
+		default:
+			//do something…………
+			time.Sleep(time.Millisecond * 1000)
+			fmt.Println(args)
+
+		}
+	}
+loop:
+
+	return 0
 }
 func main() {
+	tt := &Test{}
 	pool := &threadpool.ThreadPool{}
-	pool.Run(30,100)
-	for i := 0; i < 300; i++ {
-		pool.AddTask(test, i)
+	pool.Run(30, 100)
+	for i := 0; i < 100; i++ {
+		pool.AddTask(tt.test, i)
 	}
-	
-	pool.FriendClose()
-	fmt.Println("回收call ")
+
 	var str string
 	fmt.Scan(&str)
-	
-	pool.Get()
+	fmt.Println("回收call ")
+	pool.BadClose2()
+
 	var str2 string
 	fmt.Scan(&str2)
+	pool.Get()
+	var str3 string
+	fmt.Scan(&str3)
 }
